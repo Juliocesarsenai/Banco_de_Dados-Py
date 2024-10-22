57bfb0459f30acdc50cb65911a12274d4169782d
Juliocesarbonfim <159816707+Juliocesarsenai@users.noreply.github.com>
2024-10-22 13:43:00 +0000

    Salvar

---
+++ app/config/connection.py
@@ -0,0 +1,33 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm  import sessionmaker
+from contextlib import contextmanager
+
+#URL de conexão par BD MySQL
+
+#Parametros de Conexão com MySQL.
+
+db_user="julio"
+db_password="julio"
+db_host="localhost"
+db_port="3306"
+db_name="meu_banco_de_dados"
+#DATABASE_URL=f"mysql + pmysql://usuario:senha@host:porta/nome_bd"
+
+DATABASE_URL=f"mysql+pymysql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
+
+db=create_engine(DATABASE_URL)
+Session=sessionmaker(bind=db)
+session=Session()
+
+@contextmanager
+def get_db():
+    db=Session() #Cria uma sessão para ações no banco de dados.
+
+    try:
+        yield db #Caso a sessão realize todas as tarefas, salva a operação 
+        db.commit()
+    except Exception as erro:
+        db.rollback()#Desfaz todas as alterações em caso de erro em alguma operação
+        raise erro #Lança uma exception 
+    finally:
+        db.close()#Fecha conexão com o banco de dados
